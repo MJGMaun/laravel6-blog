706a9afa25293b42004554d2411a918efb4a1c23
Mikhaela Maun <gncrnmn@gmail.com>
2020-02-06 16:59:44 +0800

    Fixed nav action on blog posts

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <nav id="nav">
     <ul>
         <li class="{{ Request::path() === '/' ? 'active' : '' }}"><a href="/">Homepage</a></li>
-        <li class="{{ Request::is('posts/*') ? 'active' : '' }}"><a href="{{ route('posts.index') }}">Blog Posts</a></li>
+        <li class="{{ Request::is('posts/*') || Request::path() === 'posts' ? 'active' : '' }}"><a href="{{ route('posts.index') }}">Blog Posts</a></li>
     </ul>
 </nav>
